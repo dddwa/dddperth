5a376a4de50969dcb0d077870da4ac08de5219ac
Jake Ginnivan <jake@ginnivan.net>
2024-07-18 21:18:56 +0800

    Fix docker issue

--- website/app/lib/config.server.ts
+++ website/app/lib/config.server.ts
@@ -2,7 +2,6 @@ import { z } from 'zod'
 
 export const {
     SESSION_SECRET,
-    WEB_URL,
     APPLICATIONINSIGHTS_CONNECTION_STRING,
     OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
     OTEL_EXPORTER_OTLP_METRICS_ENDPOINT,
@@ -13,7 +12,6 @@ export const {
     .object({
         NODE_ENV: z.string().default('development'),
         SESSION_SECRET: z.string().default('SESSION_SECRET'),
-        WEB_URL: z.string(),
 
         APPLICATIONINSIGHTS_CONNECTION_STRING: z.string().optional(),
         OTEL_EXPORTER_OTLP_ENDPOINT: z.string().optional(),
