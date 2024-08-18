88d472bbc8ef30e6dc5863c854491f58ed9c1222
Corey Ginnivan <corey@buildkite.com>
2024-08-18 17:02:14 +0800

    Fixed svg ids

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -1,5 +1,6 @@
 import mdx from '@mdx-js/rollup'
 import { vitePlugin as remix } from '@remix-run/dev'
+import { customAlphabet } from 'nanoid'
 import rehypePrettyCode from 'rehype-pretty-code'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
@@ -10,33 +11,49 @@ import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 const prettyCodeOptions = {
-    theme: 'catppuccin-latte',
+  theme: 'catppuccin-latte',
+}
+const prefixList = new Set()
+const nanoid = customAlphabet('0123456789abcdefghijklmnopqrstuvwxyz', 10)
+const uniquePrefix = () => {
+  let prefix
+  do {
+    prefix = nanoid()
+  } while (prefixList.has(prefix))
+  return prefix
 }
 
 export default defineConfig({
-    // eslint-disable-next-line no-undef
-    root: __dirname,
-    server: {
-        port: 3800,
-        hmr: {
-            port: 3805,
-        },
+  // eslint-disable-next-line no-undef
+  root: __dirname,
+  server: {
+    port: 3800,
+    hmr: {
+      port: 3805,
     },
-    plugins: [
-        tsconfigPaths(),
-        remixDevTools(),
-        mdx({
-            remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
-            rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
-            /* jsxImportSource: …, otherOptions… */
-        }),
-        remix({
-            buildDirectory: 'build/remix',
-        }),
-        remixRoutes({
-            outDir: '.',
-            strict: true,
-        }),
-        svgr(),
-    ],
+  },
+  plugins: [
+    tsconfigPaths(),
+    remixDevTools(),
+    mdx({
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+      /* jsxImportSource: …, otherOptions… */
+    }),
+    remix({
+      buildDirectory: 'build/remix',
+    }),
+    remixRoutes({
+      outDir: '.',
+      strict: true,
+    }),
+    svgr({
+      svgrOptions: {
+        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+        svgoConfig: {
+          plugins: [{ name: 'prefixIds', params: { prefix: uniquePrefix } }],
+        },
+      },
+    }),
+  ],
 })
