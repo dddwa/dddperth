17bf25441587a60f0bd014534110b442c7ee25a7
Jake Ginnivan <jake@ginnivan.net>
2024-08-19 21:57:47 +0800

    Removing some not done links

--- website/app/components/footer/footer.tsx
+++ website/app/components/footer/footer.tsx
@@ -4,96 +4,96 @@ import { Divider, Flex, Grid, styled } from '../../../styled-system/jsx'
 import { socialsData } from '../../config/socials-data'
 
 export const Footer = () => (
-  <Flex mt={48} direction="column" gap={6} maxW="1200px" mx="auto">
-    <Divider color="#8D8DFF33" mb={6} />
-    <Grid gridTemplateColumns="repeat(4, 1fr)" width="full" gap={6}>
-      <Flex flexGrow={2}>
-        <Logo width={153} />
-      </Flex>
-      <Flex direction="column" gap={1} fontWeight="medium">
-        <styled.a href={`/agenda`} color="white" _hover={{ color: '#8282FB' }}>
-          Previous Years Sessions
-        </styled.a>
-        <styled.a href={`/speakers`} color="white" _hover={{ color: '#8282FB' }}>
-          Speakers
-        </styled.a>
-        <styled.a href={`/agenda`} color="white" _hover={{ color: '#8282FB' }}>
-          Agenda
-        </styled.a>
-        <styled.a href={`/sponsorship`} color="white" _hover={{ color: '#8282FB' }}>
-          Sponsorship
-        </styled.a>
-        <styled.a href={`/about`} color="white" _hover={{ color: '#8282FB' }}>
-          About
-        </styled.a>
-        <styled.a href={`/blog`} color="white" _hover={{ color: '#8282FB' }}>
+    <Flex mt={48} direction="column" gap={6} maxW="1200px" mx="auto">
+        <Divider color="#8D8DFF33" mb={6} />
+        <Grid gridTemplateColumns="repeat(4, 1fr)" width="full" gap={6}>
+            <Flex flexGrow={2}>
+                <Logo width={153} />
+            </Flex>
+            <Flex direction="column" gap={1} fontWeight="medium">
+                <styled.a href={`/agenda`} color="white" _hover={{ color: '#8282FB' }}>
+                    Previous Years Sessions
+                </styled.a>
+                <styled.a href={`/speakers`} color="white" _hover={{ color: '#8282FB' }}>
+                    Speakers
+                </styled.a>
+                <styled.a href={`/agenda`} color="white" _hover={{ color: '#8282FB' }}>
+                    Agenda
+                </styled.a>
+                <styled.a href={`/sponsorship`} color="white" _hover={{ color: '#8282FB' }}>
+                    Sponsorship
+                </styled.a>
+                <styled.a href={`/about`} color="white" _hover={{ color: '#8282FB' }}>
+                    About
+                </styled.a>
+                {/* <styled.a href={`/blog`} color="white" _hover={{ color: '#8282FB' }}>
           Blog
-        </styled.a>
-        <styled.a href={`/faq`} color="white" _hover={{ color: '#8282FB' }}>
-          FAQ
-        </styled.a>
-      </Flex>
-      <Flex direction="column" gap={1} fontWeight="medium">
-        <styled.a href={`/code-of-conduct`} color="white" _hover={{ color: '#8282FB' }}>
-          Code Of Conduct
-        </styled.a>
-        <styled.a href={`/terms-and-conditions`} color="white" _hover={{ color: '#8282FB' }}>
-          Terms & Conditions
-        </styled.a>
-        <styled.a href={`/venue`} color="white" _hover={{ color: '#8282FB' }}>
-          Venue
-        </styled.a>
-        <styled.a href={`/health-policy`} color="white" _hover={{ color: '#8282FB' }}>
-          Health Policy
-        </styled.a>
-        <styled.a href={`/volunteer`} color="white" _hover={{ color: '#8282FB' }}>
-          Work With Us
-        </styled.a>
-        <styled.a href={`mailto:info@dddperth.com`} color="#8282FB" _hover={{ color: 'white' }}>
-          info@dddperth.com
-        </styled.a>
-      </Flex>
-      <Flex direction="column" gap={4}>
-        <styled.p color="#C2C2FF" fontSize="2xl" lineHeight={1.2} textWrap="balance">
-          Subscribe for the latest DDD updates
-        </styled.p>
-        <styled.input
-          type="email"
-          placeholder="Email Address"
-          bg="#0E0E43"
-          color="white"
-          borderRightRadius="full"
-          px={4}
-          py={2}
-          fontSize="lg"
-          border="1px solid #8D8DFF33"
-          _placeholder={{ color: '#8282FB' }}
-          mb={4}
-        />
-        <Flex direction="row" gap={1}>
-          {socialsData.map((item) => (
-            <styled.a
-              aria-label={`Visit us on ${item.title}`}
-              key={item.link}
-              href={item.link}
-              target={`_blank`}
-              rel={`nofollow noopener`}
-              display="flex"
-              justifyContent="flex-start"
-              alignItems="center"
-            >
-              <item.icon
-                className={css({
-                  w: '8',
-                  color: '#8282FB',
-                  transition: 'colors',
-                  _hover: { color: 'white' },
-                })}
-              />
-            </styled.a>
-          ))}
-        </Flex>
-      </Flex>
-    </Grid>
-  </Flex>
+        </styled.a> */}
+                <styled.a href={`/faq`} color="white" _hover={{ color: '#8282FB' }}>
+                    FAQ
+                </styled.a>
+            </Flex>
+            <Flex direction="column" gap={1} fontWeight="medium">
+                <styled.a href={`/code-of-conduct`} color="white" _hover={{ color: '#8282FB' }}>
+                    Code Of Conduct
+                </styled.a>
+                <styled.a href={`/terms-and-conditions`} color="white" _hover={{ color: '#8282FB' }}>
+                    Terms & Conditions
+                </styled.a>
+                <styled.a href={`/venue`} color="white" _hover={{ color: '#8282FB' }}>
+                    Venue
+                </styled.a>
+                <styled.a href={`/health-policy`} color="white" _hover={{ color: '#8282FB' }}>
+                    Health Policy
+                </styled.a>
+                <styled.a href={`/volunteer`} color="white" _hover={{ color: '#8282FB' }}>
+                    Work With Us
+                </styled.a>
+                <styled.a href={`mailto:info@dddperth.com`} color="#8282FB" _hover={{ color: 'white' }}>
+                    info@dddperth.com
+                </styled.a>
+            </Flex>
+            <Flex direction="column" gap={4}>
+                <styled.p color="#C2C2FF" fontSize="2xl" lineHeight={1.2} textWrap="balance">
+                    Subscribe for the latest DDD updates
+                </styled.p>
+                <styled.input
+                    type="email"
+                    placeholder="Email Address"
+                    bg="#0E0E43"
+                    color="white"
+                    borderRightRadius="full"
+                    px={4}
+                    py={2}
+                    fontSize="lg"
+                    border="1px solid #8D8DFF33"
+                    _placeholder={{ color: '#8282FB' }}
+                    mb={4}
+                />
+                <Flex direction="row" gap={1}>
+                    {socialsData.map((item) => (
+                        <styled.a
+                            aria-label={`Visit us on ${item.title}`}
+                            key={item.link}
+                            href={item.link}
+                            target={`_blank`}
+                            rel={`nofollow noopener`}
+                            display="flex"
+                            justifyContent="flex-start"
+                            alignItems="center"
+                        >
+                            <item.icon
+                                className={css({
+                                    w: '8',
+                                    color: '#8282FB',
+                                    transition: 'colors',
+                                    _hover: { color: 'white' },
+                                })}
+                            />
+                        </styled.a>
+                    ))}
+                </Flex>
+            </Flex>
+        </Grid>
+    </Flex>
 )
--- website/app/components/header/header.tsx
+++ website/app/components/header/header.tsx
@@ -4,58 +4,58 @@ import { Box, Flex, styled } from '../../../styled-system/jsx'
 import { AppLink } from '../app-link'
 
 export function Header() {
-  return (
-    <>
-      <styled.header position="relative" bgColor="#070727" w="100%" display="flex" zIndex="10" py={4}>
-        <Box w="100%" position="relative" maxW="1200px" m="0 auto">
-          <Box w="100%" position="relative">
-            <Flex flexDir="row" justifyContent="space-between" alignItems="center">
-              <Flex flexDir="row" justifyContent="flex-start" alignItems="flex-start" md={{ p: '4' }}>
-                <AppLink
-                  aria-label={`Visit the ${conferenceConfig.name} homepage`}
-                  to={`/`}
-                  display="flex"
-                  justifyContent="flex-start"
-                  alignItems="center"
-                  h="20"
-                  px="4"
-                  md={{ px: '0', h: 'auto' }}
-                >
-                  <Logo width={153} />
-                </AppLink>
-              </Flex>
-              <Flex flexDir="row" alignItems="center" gap={12} fontWeight={600}>
-                <styled.a href="/event/speakers" color="white" _hover={{ color: '#8282FB' }}>
-                  Speakers
-                </styled.a>
-                <styled.a href="/agenda" color="white" _hover={{ color: '#8282FB' }}>
-                  Agenda
-                </styled.a>
-                <styled.a href="/blog" color="white" _hover={{ color: '#8282FB' }}>
-                  About
-                </styled.a>
-                <styled.a href="/about" color="white" _hover={{ color: '#8282FB' }}>
+    return (
+        <>
+            <styled.header position="relative" bgColor="#070727" w="100%" display="flex" zIndex="10" py={4}>
+                <Box w="100%" position="relative" maxW="1200px" m="0 auto">
+                    <Box w="100%" position="relative">
+                        <Flex flexDir="row" justifyContent="space-between" alignItems="center">
+                            <Flex flexDir="row" justifyContent="flex-start" alignItems="flex-start" md={{ p: '4' }}>
+                                <AppLink
+                                    aria-label={`Visit the ${conferenceConfig.name} homepage`}
+                                    to={`/`}
+                                    display="flex"
+                                    justifyContent="flex-start"
+                                    alignItems="center"
+                                    h="20"
+                                    px="4"
+                                    md={{ px: '0', h: 'auto' }}
+                                >
+                                    <Logo width={153} />
+                                </AppLink>
+                            </Flex>
+                            <Flex flexDir="row" alignItems="center" gap={12} fontWeight={600}>
+                                {/* <styled.a href="/event/speakers" color="white" _hover={{ color: '#8282FB' }}>
+                                    Speakers
+                                </styled.a> */}
+                                <styled.a href="/agenda" color="white" _hover={{ color: '#8282FB' }}>
+                                    Agenda
+                                </styled.a>
+                                <styled.a href="/about" color="white" _hover={{ color: '#8282FB' }}>
+                                    About
+                                </styled.a>
+                                {/* <styled.a href="/blog" color="white" _hover={{ color: '#8282FB' }}>
                   Blog
-                </styled.a>
-                <styled.a
-                  href="/about"
-                  color="#520030"
-                  _hover={{ gradientTo: '#FF52B7' }}
-                  bgGradient="to-r"
-                  gradientFrom="#FF52B7"
-                  gradientTo="#FF8273"
-                  fontWeight={600}
-                  borderRightRadius={100}
-                  py={2}
-                  px={4}
-                >
-                  Buy Tickets ↗
-                </styled.a>
-              </Flex>
-            </Flex>
-          </Box>
-        </Box>
-      </styled.header>
-    </>
-  )
+                </styled.a> */}
+                                <styled.a
+                                    href="/about"
+                                    color="#520030"
+                                    _hover={{ gradientTo: '#FF52B7' }}
+                                    bgGradient="to-r"
+                                    gradientFrom="#FF52B7"
+                                    gradientTo="#FF8273"
+                                    fontWeight={600}
+                                    borderRightRadius={100}
+                                    py={2}
+                                    px={4}
+                                >
+                                    Buy Tickets ↗
+                                </styled.a>
+                            </Flex>
+                        </Flex>
+                    </Box>
+                </Box>
+            </styled.header>
+        </>
+    )
 }
