cf860d9cd57c13bd8ef2febcf87c30e533b55d38
Jake Ginnivan <jake@ginnivan.net>
2024-08-20 14:00:18 +0800

    Mobile margins

--- website/app/components/footer/footer.tsx
+++ website/app/components/footer/footer.tsx
@@ -4,7 +4,16 @@ import { Divider, Flex, Grid, styled } from '../../../styled-system/jsx'
 import { socialsData } from '../../config/socials-data'
 
 export const Footer = () => (
-    <Flex mt={48} direction="column" gap={6} maxW="1200px" mx="auto">
+    <Flex
+        mt={48}
+        direction="column"
+        gap={6}
+        maxW="1200px"
+        mx="5"
+        xl={{
+            mx: 0,
+        }}
+    >
         <Divider color="#8D8DFF33" mb={6} />
         <Grid gridTemplateColumns="repeat(4, 1fr)" width="full" gap={6}>
             <Flex flexGrow={2}>
@@ -14,15 +23,15 @@ export const Footer = () => (
                 <styled.a href={`/agenda`} color="white" _hover={{ color: '#8282FB' }}>
                     Previous Years Sessions
                 </styled.a>
-                <styled.a href={`/speakers`} color="white" _hover={{ color: '#8282FB' }}>
+                {/* <styled.a href={`/speakers`} color="white" _hover={{ color: '#8282FB' }}>
                     Speakers
-                </styled.a>
+                </styled.a> */}
                 <styled.a href={`/agenda`} color="white" _hover={{ color: '#8282FB' }}>
                     Agenda
                 </styled.a>
-                <styled.a href={`/sponsorship`} color="white" _hover={{ color: '#8282FB' }}>
+                {/* <styled.a href={`/sponsorship`} color="white" _hover={{ color: '#8282FB' }}>
                     Sponsorship
-                </styled.a>
+                </styled.a> */}
                 <styled.a href={`/about`} color="white" _hover={{ color: '#8282FB' }}>
                     About
                 </styled.a>
@@ -36,9 +45,6 @@ export const Footer = () => (
             <Flex direction="column" gap={1} fontWeight="medium">
                 <styled.a href={`/code-of-conduct`} color="white" _hover={{ color: '#8282FB' }}>
                     Code Of Conduct
-                </styled.a>
-                <styled.a href={`/terms-and-conditions`} color="white" _hover={{ color: '#8282FB' }}>
-                    Terms & Conditions
                 </styled.a>
                 <styled.a href={`/venue`} color="white" _hover={{ color: '#8282FB' }}>
                     Venue
@@ -54,7 +60,7 @@ export const Footer = () => (
                 </styled.a>
             </Flex>
             <Flex direction="column" gap={4}>
-                <styled.p color="#C2C2FF" fontSize="2xl" lineHeight={1.2} textWrap="balance">
+                {/* <styled.p color="#C2C2FF" fontSize="2xl" lineHeight={1.2} textWrap="balance">
                     Subscribe for the latest DDD updates
                 </styled.p>
                 <styled.input
@@ -69,7 +75,7 @@ export const Footer = () => (
                     border="1px solid #8D8DFF33"
                     _placeholder={{ color: '#8282FB' }}
                     mb={4}
-                />
+                /> */}
                 <Flex direction="row" gap={1}>
                     {socialsData.map((item) => (
                         <styled.a
