db53a110d66009d758a0c5fbe596c43b15c642fb
Jet760 <jessicarturner00@gmail.com>
2026-06-28 22:41:22 +0800

    add runsheets page

--- core/website/safe-routes.d.ts
+++ core/website/safe-routes.d.ts
@@ -118,6 +118,10 @@ declare module "safe-routes" {
       params: never,
       query: ExportedQuery<import('app/routes/[robots.txt].js').SearchParams>,
     },
+    "/runsheets": {
+      params: never,
+      query: ExportedQuery<import('app/routes/_layout.runsheets._index.js').SearchParams>,
+    },
     "/sitemap.xml": {
       params: never,
       query: ExportedQuery<import('app/routes/sitemap[.xml].js').SearchParams>,
@@ -159,11 +163,12 @@ declare module "safe-routes" {
             | 'routes/_layout'
             | 'routes/_layout.agenda.$year.talk.$sessionId'
             | 'routes/_layout.sponsors.($year)'
+            | 'routes/_layout.runsheet._index'
             | 'routes/_layout.agenda.($year)'
             | 'routes/_layout.blog._index'
             | 'routes/_layout.blog.$slug'
-            | 'routes/_layout._index'
             | 'routes/_layout.voting'
+            | 'routes/_layout._index'
             | 'routes/_layout.app'
             | 'routes/_layout.$'
             | 'routes/admin'
@@ -172,8 +177,8 @@ declare module "safe-routes" {
             | 'routes/admin.dashboard'
             | 'routes/admin.settings'
             | 'routes/admin.content'
-            | 'routes/admin._index'
-            | 'routes/admin.voting';
+            | 'routes/admin.voting'
+            | 'routes/admin._index';
 
   export function $path<
     Route extends keyof Routes,
