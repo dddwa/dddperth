fe09599e4ad36af97d9782b4b50c09e39d6c0ca6
Jet760 <jessicarturner00@gmail.com>
2026-07-07 00:23:32 +0800

    add api call to atlassian to get issues

--- core/website/app/routes/_layout.runsheets._index.tsx
+++ core/website/app/routes/_layout.runsheets._index.tsx
@@ -1,22 +1,178 @@
+import console from 'node:console'
 import { data, useLoaderData } from 'react-router'
+import { z } from 'zod'
 import { AdminLayout } from '~/components/admin-layout'
-import { styled } from '~/styled-system/jsx'
+import { Button } from '~/components/ui/button'
+import { Flex, styled } from '~/styled-system/jsx'
 import type { Route } from './+types/_layout.runsheets._index'
 
+export interface Issue {
+    id: number
+    // parent: Issue;
+    // project: Project;
+    key: string
+    summary: string
+    description: string
+    issueType: IssueType
+    status: IssueStatus
+    labels: string[]
+
+    // Custom fields - accessed via ID or key
+    [customFieldId: string]: any
+}
+export const issueSchema = z.object({
+    id: z.string(),
+    self: z.string(),
+    key: z.string(),
+    fields: z.object({
+        summary: z.string(),
+        issuetype: z.object({
+            id: z.string(),
+            name: z.string(),
+            description: z.string(),
+        }),
+        labels: z.array(z.string()),
+        status: z.object({
+            id: z.string(),
+            name: z.string(),
+            description: z.string(),
+        }),
+        description: z.object().nullable(),
+        customfield_10131: z.string().nullable(), // Role Instructions
+        customfield_10132: z.array(z.string()).nullable(), // Volunteer Team
+        customfield_10133: z.string(), // Item End Time
+        customfield_10134: z.string(), // Item Start Time
+        customfield_10135: z.array(z.string()), // Location
+    }),
+})
+
+export interface IssueType {
+    id: number
+    name: string
+    description: string
+    // iconUrl: string;
+    // hierarchyLevel: number;
+    // properties: EntityProperties;
+}
+
+export interface IssueStatus {
+    id: number
+    name: string
+    description: string
+    category: IssueStatusCategory
+}
+
+export interface IssueStatusCategory {
+    id: number
+    key: string
+    name: string
+    colorName: string
+}
+
+export const jsonSchema = z.object({
+    issues: z.array(z.object({ id: z.string() })),
+    isLast: z.boolean(),
+})
+
+export const bulkIssuesSchema = z.object({
+    issues: z.array(issueSchema),
+})
+
 export async function loader({ context }: Route.LoaderArgs) {
-    return data({ test: 'Meow' })
+    // get ids of issues in team
+    const fetchedIds = await fetch(
+        'https://dddperth.atlassian.net/rest/api/3/search/jql?jql=project %3D VOL AND type %3D "Run Sheet Item" &type=issue&product=jira',
+        // 'https://dddperth.atlassian.net/rest/api/3/search/jql?jql=project %3D VOL AND type %3D "Run Sheet Item" AND "Volunteer Team[Labels]" %3D team-1&type=issue&product=jira',
+        {
+            method: 'GET',
+            headers: {
+                Authorization: `Basic ${Buffer.from('email@email.com:api_key').toString('base64')}`,
+                Accept: 'application/json',
+            },
+        },
+    )
+    if (!fetchedIds.ok) {
+        throw new Error('Error fetching issue ids, responded with status: ' + fetchedIds.status)
+    }
+    const jsonIds = await fetchedIds.json()
+    console.log(jsonIds)
+    const idList = jsonSchema.parse(jsonIds).issues
+
+    if (!idList) {
+        throw new Error('Error parsing issue ids')
+    }
+    const issueIds = []
+    for (const issue in idList) {
+        issueIds.push(`"${idList[issue].id}"`)
+        console.log(issueIds)
+    }
+
+    // get issue details
+    if (issueIds.length > 0) {
+        const bodyData = `{
+          "expand": [
+            "names"
+          ],
+          "fields": [
+            "issuetype",
+            "labels",
+            "status",
+            "description",
+            "customfield_10131",
+            "customfield_10132",
+            "customfield_10133",
+            "customfield_10134",
+            "customfield_10135",
+            "summary"
+          ],
+          "fieldsByKeys": false,
+          "issueIdsOrKeys": [${issueIds}],
+          "properties": []
+        }`
+        const fetchedIssues = await fetch('https://dddperth.atlassian.net/rest/api/3/issue/bulkfetch', {
+            method: 'POST',
+            headers: {
+                Authorization: `Basic ${Buffer.from('email@email.com:api_key').toString('base64')}`,
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: bodyData,
+        })
+        if (!fetchedIssues.ok) {
+            throw new Error('Error fetching issues, responded with status: ' + fetchedIssues.status)
+        }
+        const issueJson = await fetchedIssues.json()
+        console.log(issueJson)
+        const issues = bulkIssuesSchema.parse(issueJson).issues
+
+        return data(issues)
+    }
 }
 
 export default function Index() {
-    const { test } = useLoaderData<typeof loader>()
+    const issues = useLoaderData<typeof loader>()
+    // if (issues) console.log(issues[0].fields)
 
     return (
         <>
             <AdminLayout heading="Runsheets">
-                <p>Hi </p>
+                <Flex>
+                    <Button
+                        // type="submit"
+                        // disabled={navigation.state === 'submitting'}
+                        variant="solid"
+                        size="sm"
+                    >
+                        Team 1
+                    </Button>
+                    <Button>Team 2</Button>
+                </Flex>
                 <styled.table width="full" fontSize="sm">
                     <thead>
                         <tr>
+                            <styled.th textAlign="left" p="2" border="admin-subtle">
+                                Summary
+                            </styled.th>
                             <styled.th textAlign="left" p="2" border="admin-subtle">
                                 Start Time
                             </styled.th>
@@ -24,10 +180,10 @@ export default function Index() {
                                 End Time
                             </styled.th>
                             <styled.th textAlign="left" p="2" border="admin-subtle">
-                                Summary
+                                Location
                             </styled.th>
                             <styled.th textAlign="left" p="2" border="admin-subtle">
-                                Location
+                                Team
                             </styled.th>
                             <styled.th textAlign="left" p="2" border="admin-subtle">
                                 Role Instructions
@@ -35,40 +191,30 @@ export default function Index() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr key={0}>
-                            <styled.td p="2" border="admin-subtle">
-                                06:30
-                            </styled.td>
-                            <styled.td p="2" border="admin-subtle">
-                                17:00
-                            </styled.td>
-                            <styled.td p="2" border="admin-subtle">
-                                3
-                            </styled.td>
-                            <styled.td p="2" border="admin-subtle">
-                                4
-                            </styled.td>
-                            <styled.td p="2" border="admin-subtle">
-                                5
-                            </styled.td>
-                        </tr>
-                        <tr key={1}>
-                            <styled.td p="2" border="admin-subtle">
-                                08:30
-                            </styled.td>
-                            <styled.td p="2" border="admin-subtle">
-                                17:00
-                            </styled.td>
-                            <styled.td p="2" border="admin-subtle">
-                                8
-                            </styled.td>
-                            <styled.td p="2" border="admin-subtle">
-                                9
-                            </styled.td>
-                            <styled.td p="2" border="admin-subtle">
-                                10
-                            </styled.td>
-                        </tr>
+                        {issues?.map((issue) => {
+                            return (
+                                <tr key={issue.id}>
+                                    <styled.td p="2" border="admin-subtle">
+                                        {issue.fields.summary}
+                                    </styled.td>
+                                    <styled.td p="2" border="admin-subtle">
+                                        {issue.fields.customfield_10134}
+                                    </styled.td>
+                                    <styled.td p="2" border="admin-subtle">
+                                        {issue.fields.customfield_10133}
+                                    </styled.td>
+                                    <styled.td p="2" border="admin-subtle">
+                                        {issue.fields.customfield_10135}
+                                    </styled.td>
+                                    <styled.td p="2" border="admin-subtle">
+                                        {issue.fields.customfield_10132}
+                                    </styled.td>
+                                    <styled.td p="2" border="admin-subtle">
+                                        {issue.fields.customfield_10131}
+                                    </styled.td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </styled.table>
             </AdminLayout>
